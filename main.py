32a726b6357f9f088f790d9aa495464ae64b85f0
pengly007 <pengly666888@gmail.com>
2022-10-25 22:54:41 +0700

    renew

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.locals import KEYDOWN, QUIT, K_SPACE, K_q, K_e
+import random
 from man import Man 
 from bullets import Bullets 
 from zombie import Zombies 
@@ -7,8 +9,10 @@ from button import Button
 pygame.init()
 WIDTH, HEIGHT = 640, 480
 win = pygame.display.set_mode((WIDTH, HEIGHT), pygame.NOFRAME)
-
+pygame.mixer.init()
 clock = pygame.time.Clock()
 FPS = 100
+
+gunshot_sound = pygame.mixer.Sound
 
 #images**************
